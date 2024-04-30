e88a4251eaefcac9c676f4372511fdfc43de98a1
NitayD <nitayd2001@gmail.com>
2024-04-30 14:52:02 +0500

    fix font load script

--- src/utils/loadFont.js
+++ src/utils/loadFont.js
@@ -1,11 +1,10 @@
-
 export default function loadFont(fontName, woffUrl, woff2Url) {
   if (typeof window === "undefined") return;
   var nua = window.navigator.userAgent;
   var noSupport = !window.addEventListener || (nua.match(/(Android (2|3|4.0|4.1|4.2|4.3))|(Opera (Mini|Mobi))/) && !nua.match(/Chrome/))
-  if (noSupport) { return ; }
+  if (noSupport) {return}
   var loSto = {};
-  try {loSto = localStorage || {}; } catch (ex) {}
+  try {loSto = localStorage || {};} catch (ex) {}
   var localStoragePrefix = "x-font-" + fontName;
   var localStorageUrlKey = localStoragePrefix + "url";
   var localStorageCssKey = localStoragePrefix + "css";
@@ -29,9 +28,9 @@ export default function loadFont(fontName, woffUrl, woff2Url) {
     request.send();
   }
 }
-function supportsWoff2() {
-  if (!window.FontFace) { return false; }
-  var f = new FontFace("t", "url(\"data:application/font-woff2,\") format(\"woff2\")", {});
-  f.load().catch(()=>{});
-  return f.status === "loading" || f.status === "loaded";
+function supportsWoff2(){
+	if(!("FontFace" in window)) return false;
+	var f = new FontFace('t', 'url( "data:application/font-woff2;base64,d09GMgABAAAAAADwAAoAAAAAAiQAAACoAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAABmAALAogOAE2AiQDBgsGAAQgBSAHIBuDAciO1EZ3I/mL5/+5/rfPnTt9/9Qa8H4cUUZxaRbh36LiKJoVh61XGzw6ufkpoeZBW4KphwFYIJGHB4LAY4hby++gW+6N1EN94I49v86yCpUdYgqeZrOWN34CMQg2tAmthdli0eePIwAKNIIRS4AGZFzdX9lbBUAQlm//f262/61o8PlYO/D1/X4FrWFFgdCQD9DpGJSxmFyjOAGUU4P0qigcNb82GAAA" ) format( "woff2" )', {});
+	f.load()['catch'](function() {});
+	return f.status == 'loading' || f.status == 'loaded';
 }
