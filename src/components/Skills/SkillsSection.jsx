0956c6fdcdb44ca077055a9292fbff2346423697
Menonro <nitayd2001@gmai.com>
2025-06-15 09:39:04 +0500

    chore: update version to 1.1.0 and remove unused dependencies
    feat: add Git SVG and WEBP images for skills section
    
    fix: center align skills section layout
    
    refactor: update skills data with new PHP and DevOps entries
    
    style: adjust global SASS imports to use @use syntax

--- src/components/Skills/SkillsSection.jsx
+++ src/components/Skills/SkillsSection.jsx
@@ -8,7 +8,7 @@ import Section from '../Base/Section'
 export default function SkillsSection() {
   return (
     <Section id='skills' title='Навыки'>
-      <Row className='g-5'>
+      <Row className='g-5 justify-content-center'>
         {
           skillsList.map(skill => (
             <Col xs="12" md="6" lg="4" key={`skill-block-${skill.title}`}>
